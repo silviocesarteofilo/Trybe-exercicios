8f8ac711424b91dd1823cf10c337e827831c5061
Silvio <silviocesarat@hotmail.com>
2023-03-28 17:17:06 -0300

    adicionando exercicios

---
+++ modulo1/secao3-introducao-javascript/dia1-introducao-javascript/ex3.js
@@ -0,0 +1,13 @@
+let a = 60;
+let b = 300;
+let c = 1300;
+
+if(a > b && a > c) {
+    console.log('O maior é:' + a);
+}
+else if(b > a && b > c) {
+    console.log('O maior é:' + b + ' (b)');
+}
+else{
+    console.log('O maior é:' + c + ' (c)');
+};
---
+++ modulo1/secao3-introducao-javascript/dia1-introducao-javascript/ex4.js
@@ -0,0 +1,9 @@
+let number = 0.5;
+
+if(number > 0) {
+    console.log('o ' + number + ' é positivo');
+}
+else if(number === 0) {
+    console.log('o ' + number + ' é zero');
+}
+else{console.log('o ' + number + ' é negativo');};
